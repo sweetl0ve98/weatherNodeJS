b606396d1705bb8952e0cff7a6eee44977c62c4e
Syn <loveparadise98@gmail.com>
2021-07-04 04:09:36 +0700

    define port

--- src/app.js
+++ src/app.js
@@ -100,6 +100,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is up on port ${process.env.PORT || 3000}.`)
 })
